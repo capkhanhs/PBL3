e110f8f0c5376b826ee6e5ef05456264cc9d25fc
Lenovo <Lenovo@C2K>
2025-04-19 19:05:23 +0700

    Add project files.

---
+++ PBL/Login.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace PBL
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.pn_login = new System.Windows.Forms.Panel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.txt_pw = new System.Windows.Forms.TextBox();
+            this.pic_pw = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txt_un = new System.Windows.Forms.TextBox();
+            this.pic_0 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btn_dn = new System.Windows.Forms.Button();
+            this.pn_login.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_pw)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pn_login
+            // 
+            this.pn_login.BackColor = System.Drawing.Color.Transparent;
+            this.pn_login.Controls.Add(this.btn_dn);
+            this.pn_login.Controls.Add(this.linkLabel1);
+            this.pn_login.Controls.Add(this.txt_pw);
+            this.pn_login.Controls.Add(this.pic_pw);
+            this.pn_login.Controls.Add(this.pictureBox1);
+            this.pn_login.Controls.Add(this.txt_un);
+            this.pn_login.Location = new System.Drawing.Point(380, 45);
+            this.pn_login.Name = "pn_login";
+            this.pn_login.Size = new System.Drawing.Size(282, 195);
+            this.pn_login.TabIndex = 0;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.LinkColor = System.Drawing.Color.AliceBlue;
+            this.linkLabel1.Location = new System.Drawing.Point(123, 123);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(156, 22);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Forget Password?";
+            // 
+            // txt_pw
+            // 
+            this.txt_pw.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_pw.Location = new System.Drawing.Point(71, 85);
+            this.txt_pw.Name = "txt_pw";
+            this.txt_pw.PasswordChar = '*';
+            this.txt_pw.Size = new System.Drawing.Size(208, 35);
+            this.txt_pw.TabIndex = 4;
+            // 
+            // pic_pw
+            // 
+            this.pic_pw.BackgroundImage = global::PBL.Properties.Resources.Password;
+            this.pic_pw.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pic_pw.Location = new System.Drawing.Point(0, 70);
+            this.pic_pw.Name = "pic_pw";
+            this.pic_pw.Size = new System.Drawing.Size(65, 50);
+            this.pic_pw.TabIndex = 3;
+            this.pic_pw.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(65, 43);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txt_un
+            // 
+            this.txt_un.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_un.Location = new System.Drawing.Point(71, 11);
+            this.txt_un.Name = "txt_un";
+            this.txt_un.Size = new System.Drawing.Size(208, 35);
+            this.txt_un.TabIndex = 0;
+            // 
+            // pic_0
+            // 
+            this.pic_0.BackColor = System.Drawing.Color.Transparent;
+            this.pic_0.BackgroundImage = global::PBL.Properties.Resources.Icon;
+            this.pic_0.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pic_0.Location = new System.Drawing.Point(-1, -1);
+            this.pic_0.Name = "pic_0";
+            this.pic_0.Size = new System.Drawing.Size(392, 274);
+            this.pic_0.TabIndex = 1;
+            this.pic_0.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = global::PBL.Properties.Resources.logo1;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(-1, 246);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(432, 101);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btn_dn
+            // 
+            this.btn_dn.BackColor = System.Drawing.Color.Transparent;
+            this.btn_dn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_dn.Location = new System.Drawing.Point(82, 155);
+            this.btn_dn.Name = "btn_dn";
+            this.btn_dn.Size = new System.Drawing.Size(146, 40);
+            this.btn_dn.TabIndex = 6;
+            this.btn_dn.Text = "Đăng Nhập";
+            this.btn_dn.UseVisualStyleBackColor = false;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::PBL.Properties.Resources.WallPaper;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(685, 335);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pic_0);
+            this.Controls.Add(this.pn_login);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Login";
+            this.Text = "Đăng Nhập";
+            this.pn_login.ResumeLayout(false);
+            this.pn_login.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_pw)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pn_login;
+        private System.Windows.Forms.TextBox txt_un;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pic_pw;
+        private System.Windows.Forms.TextBox txt_pw;
+        private System.Windows.Forms.PictureBox pic_0;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btn_dn;
+    }
+}
